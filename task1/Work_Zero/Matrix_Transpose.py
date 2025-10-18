13ccc088cec1587cc8e5508bf7aa95951b31120e
Xzms0 <xzms0000@gmail.com>
2025-10-18 19:13:12 +0800

    完成 作业零-矩阵转置

---
+++ task1/Work_Zero/Matrix_Transpose.py
@@ -0,0 +1,4 @@
+matrix_before=[[1 for column in range(10)] for row in range(5)]
+print(f"Before transpose: {matrix_before}")
+matrix_after=[[row[column] for row in matrix_before] for column in matrix_before[0]]
+print(f"After transpose: {matrix_after}")
