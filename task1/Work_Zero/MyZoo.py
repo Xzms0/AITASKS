a063ce97cd07cb8a21894c3ef10f2a4baf7a4e6b
Xzms0 <xzms0000@gmail.com>
2025-10-18 20:16:01 +0800

    完成 作业零-MyZoo

---
+++ task1/Work_Zero/MyZoo.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+class MyZoo:
+
+    def __init__(self,animals={}):
+        self.animals=animals
+        print("My Zoo!")
+        self.animals_list=list(self.animals.keys())
+        self.animals_number=list(self.animals.values())
+
+    def __str__(self):
+        zoo_list=""
+        for i in self.animals_list:
+            j=self.animals[i]
+            zoo_list+=f"{i}-{j} "
+        return zoo_list
+
+    def __len__(self):
+        return sum(self.animals_number)
+    
+    def __eq__(self, another:MyZoo):
+        self.animals_list.sort()
+        another.animals_list.sort()
+        if self.animals_list == another.animals_list:
+            return True
+        return False
+    
+    def __ne__(self, another:MyZoo):
+        self.animals_list.sort()
+        another.animals_list.sort()
+        if self.animals_list != another.animals_list:
+            return True
+        return False
+
+my_zoo1=MyZoo({"dog":5,"cat":7})
+my_zoo2=MyZoo({"cat":3,"dog":10})
+print(f"Zoo1's Len: {len(my_zoo1)}")
+print(f"Zoo2's Len: {len(my_zoo2)}")
+print(f"Zoo1's Print: {my_zoo1}")
+print(f"Zoo2's Print: {my_zoo2}")
+print(f"Zoo1 == Zoo2: {my_zoo1==my_zoo2}")
+print(f"Zoo1 != Zoo2: {my_zoo1!=my_zoo2}")
