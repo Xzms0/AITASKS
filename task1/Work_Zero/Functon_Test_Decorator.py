e2d1e393ab6e33c5555ad7b07d4757987c1f5026
Xzms0 <xzms0000@gmail.com>
2025-10-18 17:36:04 +0800

    完成 作业零-装饰器

---
+++ task1/Work_Zero/Functon_Test_Decorator.py
@@ -0,0 +1,27 @@
+import time
+
+def function_test(func):
+    def wrapper(*args, **kwargs):
+        print(f"Function {func.__name__} is running.")
+        time_start=time.perf_counter()
+        result=func(*args, **kwargs)
+        time_end=time.perf_counter()
+        time_value=time_end-time_start
+        print(f"The function takes {time_value} seconds.")
+        return result
+    return wrapper
+
+@function_test
+def useless():
+    sum=0
+    for i in range(1,1000):
+        for j in range(1,1000):
+            sum+=(i%j)
+    print(f"The result is {sum}.")
+
+useless()
+
+
+
+
+
