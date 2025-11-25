66fd1b26b07a0c9d6b25dc75bc8d4cf29bc5591d
Xzer0 <xzms0000@gmail.com>
2025-11-25 23:36:00 +0800

    update: task4-kNN and test result

--- task4/k_NN/k_nn.py
+++ task4/k_NN/k_nn.py
@@ -4,11 +4,36 @@ import random
 
 import numpy as np
 
+ROOT_DIR = Path(__file__).absolute().parent
 CIFAR_DIR = Path(__file__).absolute().parent.parent / "cifar_10"
+ADAPTIVE_BATCH = 10**6
+
 METRIC = 'L2'
 DATA_BATCH = 5
-VAL_SAMPLE = 1000
-K = range(1,16)
+VAL_SAMPLE = 5000
+TEST_SAMPLE = 10000
+TRAIN_SAMPLE = 50000
+K = range(1,21)
+
+
+class Logger:
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.record = {}
+
+        with open(ROOT_DIR / self.file_name, 'a', encoding='utf-8') as file:
+            file.write('-'*36+'\n')
+            file.write(f"METRIC: {METRIC}, TRAIN_SAMPLE: {TRAIN_SAMPLE}\nTEST_SAMPLE: {TEST_SAMPLE}, VAL_SAMPLE: {VAL_SAMPLE}\n\n")
+    
+    def reader(self):
+        pass
+
+
+    def writer(self, k: int, acc: float, msg="Val"):
+        print(f"{msg}_K: {k}, Accuracy: {acc*100:.2f}%")
+        with open(ROOT_DIR / self.file_name, 'a', encoding='utf-8') as file:
+            file.write(f">>> {msg}_K: {k}, Accuracy: {acc*100:.2f}%\n")
+
 
 class kNN:
     def __init__(self, k=8):
@@ -23,35 +48,47 @@ class kNN:
         self.test_data = None
         self.test_label = None
     
-
-    def _distances(self, train_data, test_data, pieces):
-        train_data = np.array(train_data[np.newaxis,:,])
-        test_data = np.array(test_data[:,np.newaxis,])
+    
+    def distances(self, train_data=None, test_data=None, pieces=None):
+        if train_data is None: 
+            train_data = self.train_data
+        if test_data is None: 
+            test_data = self.test_data
 
         test_num = test_data.shape[0]
-        distances_list = []
-        for start in range(0, test_num, pieces):
+        train_num = train_data.shape[0]
 
+        if pieces is None:
+            pieces = ADAPTIVE_BATCH // (train_num * train_data.itemsize)
+
+        distances = np.zeros((test_num, train_num))
+        for start in range(0, test_num, pieces):
             end = min(start + pieces, test_num)
+            process = end / test_num
+            process_show = int(process * 24)
 
             if METRIC == 'L1':
-                distances_array = np.array(np.sum(np.abs(test_data[start:end,:,] - train_data), axis=2))
+                distances_array = np.array(np.sum(np.abs(test_data[start:end,np.newaxis,] - train_data[np.newaxis,:,]), axis=2))
             elif METRIC == 'L2':
-                distances_array = np.sqrt(np.sum((test_data[start:end,:,] - train_data)**2, axis=2))
+                distances_array = np.sqrt(np.sum((test_data[start:end,np.newaxis,] - train_data[np.newaxis,:,])**2, axis=2))
 
             print(f"-> Got {METRIC} distances_{start//pieces+1} array shaped {distances_array.shape}")
-            distances_list.append(distances_array)
+            print(f"Process: {process * 100:.2f}% " + "/" * process_show + "-" * (24 - process_show), end='\r')
+            distances[start:end,:] = distances_array
 
-        distances = self._to_array(distances_list, msg=f'{METRIC} distances_total', axis=0)
+        print(f"-> Got distance_total array shaped {distances.shape}")
         return distances
 
 
-    def _to_array(self, data_list: list, msg='an', axis=0):
-        array = np.concatenate(data_list, axis=axis)
-        print(f"-> Got {msg} array shaped {array.shape}")
-        return array
+    def normalize(self):
+        if self.train_data is not None:
+            self.train_data = self.train_data.astype(np.float32) / 255.0
+        if self.test_data is not None:
+            self.test_data = self.test_data.astype(np.float32) / 255.0
+
+        print("Data is normalized to float32...")
     
-    
+
     def loads(self):
         def unpickle(file_name: str) -> dict:
             with open(CIFAR_DIR / file_name,'rb') as file:
@@ -60,26 +97,32 @@ class kNN:
             print(f"-> Loaded {file_name} with {dict.keys()}")
             return dict
         
+        def to_array(data_list: list, msg='an', axis=0):
+            array = np.concatenate(data_list, axis=axis)
+            print(f"-> Got {msg} array shaped {array.shape}")
+            return array
+        
         print("Start loading cifar-10 batches...")
         self.meta_dict = unpickle('batches.meta')
         self.test_dict = unpickle('test_batch')
         for i in range(DATA_BATCH):
             self.train_dict.append(unpickle(f'data_batch_{i+1}'))
 
-        self.train_data = self._to_array([self.train_dict[i].get(b'data') for i in range(len(self.train_dict))], msg="train_data")
-        self.train_label = self._to_array([self.train_dict[i].get(b'labels') for i in range(len(self.train_dict))], msg="train_label")
+        self.train_data = to_array([self.train_dict[i].get(b'data') for i in range(len(self.train_dict))], msg="train_data")
+        self.train_label = to_array([self.train_dict[i].get(b'labels') for i in range(len(self.train_dict))], msg="train_label")
 
-        self.test_data = self._to_array([self.test_dict.get(b'data')], msg="test_data")
-        self.test_label = self._to_array([self.test_dict.get(b'labels')], msg="test_label")
+        self.test_data = to_array([self.test_dict.get(b'data')], msg="test_data")
+        self.test_label = to_array([self.test_dict.get(b'labels')], msg="test_label")
 
 
     def train(self, val_sample=None):
-        self.train_data = self.train_data
-        self.train_label = self.train_label
+        self.train_data = self.train_data[:TRAIN_SAMPLE]
+        self.train_label = self.train_label[:TRAIN_SAMPLE]
 
         if val_sample is not None:
-            print(f"\nStart validation with {val_sample} samples...")
+            print(f"Start validation with {val_sample} samples...")
             index = list(range(self.train_data.shape[0]))
+            val_logger = Logger('val_accuracy.txt')
 
             random.seed(10)
             random.shuffle(index)
@@ -98,34 +141,32 @@ class kNN:
             print(f"-> Got val_label array shaped {val_label.shape}")
 
             best_k = [0, 0]
+            val_distances = self.distances(train_data=trn_data, test_data=val_data)
             for k in K:
-                self.k = k
-                print(f"\nValidating with K valued {self.k}...")
-                val_predict = self.predict(train_data=trn_data, train_label=trn_label, test_data=val_data, pieces=25)
+                val_predict = self.predict(distances=val_distances, train_label=trn_label, k=k)
                 val_acc = self.accuracy(val_predict, val_label)
 
                 if val_acc > best_k[1]:
-                    best_k = [self.k, val_acc]
-                print(f"The {self.k} validation got {val_acc*100:.2f}% accuracy.")
+                    best_k = [k, val_acc]
+
+                val_logger.writer(k, val_acc, msg='Val')
 
             self.k = best_k[0]
-            print(f"\nEnd validation with best K valued {self.k}")
+            val_logger.writer(best_k[0], best_k[1], msg='Best')
 
 
-    def predict(self, train_data=None, train_label=None, test_data=None, pieces=20):
-        if train_data is None:
-            train_data = self.train_data
-        if test_data is None:
-            test_data = self.test_data
+    def predict(self, distances, train_label=None, k=None):
         if train_label is None:
             train_label =self.train_label
+        if k is None: 
+            k = self.k
         
-        distances = self._distances(train_data, test_data, pieces=pieces)
-        min_indexs = np.argpartition(distances, self.k, axis=1)[:,:self.k]
+        min_indexs = np.argpartition(distances, k, axis=1)[:,:k]
         predict_k = np.array(train_label[min_indexs])
-        predictions = np.array([np.bincount(sample).argmax() for sample in predict_k])
-        print(f"-> Got predictions array shaped {predictions.shape}")
-        return predictions
+        predict = np.array([np.bincount(sample).argmax() for sample in predict_k])
+
+        #print(f"-> Got predict array shaped {predict.shape}")
+        return predict
     
 
     def accuracy(self, predict, labels=None):
@@ -139,9 +180,25 @@ class kNN:
 if __name__ == '__main__':
     knn = kNN()
     knn.loads()
+    knn.normalize()
     knn.train(VAL_SAMPLE)
-    predict = knn.predict(test_data=knn.test_data[:2000])
-    acc = knn.accuracy(predict, labels=knn.test_label[:2000])
-    print(f"The final predict got {acc*100:.2f}% accuracy.")
+
+    best_k = [0, 0]
+    test_acc = 0
+    test_logger = Logger('test_accuracy.txt')
+    distances = knn.distances(test_data=knn.test_data[:TEST_SAMPLE])
+    for k in K:
+        predict = knn.predict(distances=distances, k=k)
+        acc = knn.accuracy(predict, labels=knn.test_label[:TEST_SAMPLE])
+
+        if acc > best_k[1]:
+            best_k = [k, acc]
+        if k == knn.k:
+            test_acc = acc
+
+        test_logger.writer(k, acc, msg='Test')
+
+    test_logger.writer(best_k[0], best_k[1], msg='Best')
+    test_logger.writer(knn.k, test_acc, msg='Val')
 
 
